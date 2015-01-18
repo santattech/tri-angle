4ea1ce9ee2057095ebe6b447862c1994c3e0ef92
Santanu <santattech@gmail.com>
2015-01-18 23:13:39 +0530

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Triapp::Application.config.session_store :cookie_store, key: '_triapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Triapp::Application.config.session_store :active_record_store
