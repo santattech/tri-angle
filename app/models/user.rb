89e231f810105015eb4e3f4303ca1dc532507158
Santanu <santattech@gmail.com>
2015-02-24 23:10:09 +0530

    [#4], schema for user has many roles association added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   validates :email, presence: true
+
+  has_many :user_roles
+  has_many :roles, through: :user_roles
 end
